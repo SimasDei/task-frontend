3ea301de87ba9e77e1562eb2605c3d31f27cd3d1
SimasDei <simondeiman@gmail.com>
2019-01-29 01:33:34 +0200

    Updates

---
+++ precache-manifest.3ab6c06717a161faba8b449b034ce0da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0db0ecbb4ccddcf12b2",
+    "url": "/static/css/main.fb689309.chunk.css"
+  },
+  {
+    "revision": "b0db0ecbb4ccddcf12b2",
+    "url": "/static/js/main.b0db0ecb.chunk.js"
+  },
+  {
+    "revision": "40f1e39a9f463204e120",
+    "url": "/static/js/1.40f1e39a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7c0b8a4cdc0f57bbe917b8b846274a01",
+    "url": "/static/media/background.7c0b8a4c.jpg"
+  },
+  {
+    "revision": "b3eb6a37c6dd071bd55647338ee78b52",
+    "url": "/static/media/logo.b3eb6a37.svg"
+  },
+  {
+    "revision": "fcca720a7212374b8410de1db9f1d7b3",
+    "url": "/index.html"
+  }
+];
