8655d9697cb55179b59ae0db1434e6cbd0643bdf
SimasDei <simondeiman@gmail.com>
2019-01-29 01:51:31 +0200

    Updates

---
+++ precache-manifest.f57cd5cdfa5a2feda3330a600312b08e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e94f883af5ec80b18e6",
+    "url": "/task-frontend/static/css/main.f158370a.chunk.css"
+  },
+  {
+    "revision": "2e94f883af5ec80b18e6",
+    "url": "/task-frontend/static/js/main.2e94f883.chunk.js"
+  },
+  {
+    "revision": "40f1e39a9f463204e120",
+    "url": "/task-frontend/static/js/1.40f1e39a.chunk.js"
+  },
+  {
+    "revision": "145f7517156f77042fee",
+    "url": "/task-frontend/static/js/runtime~main.145f7517.js"
+  },
+  {
+    "revision": "7c0b8a4cdc0f57bbe917b8b846274a01",
+    "url": "/task-frontend/static/media/background.7c0b8a4c.jpg"
+  },
+  {
+    "revision": "b3eb6a37c6dd071bd55647338ee78b52",
+    "url": "/task-frontend/static/media/logo.b3eb6a37.svg"
+  },
+  {
+    "revision": "53dcd75ab8d1de2064c909976b069a67",
+    "url": "/task-frontend/index.html"
+  }
+];
